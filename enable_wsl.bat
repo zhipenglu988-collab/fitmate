c9b41dc92610d8aff1628c1bafeb2b7e4cd593a6
zhipenglu988-collab <zhipenglu988@gmail.com>
2026-06-03 04:58:00 +0800

    Add enable_wsl.bat

---
+++ enable_wsl.bat
@@ -0,0 +1,7 @@
+@echo off
+echo Enabling Windows Subsystem for Linux...
+dism /online /enable-feature /featurename:Microsoft-Windows-Subsystem-Linux /all
+echo.
+echo WSL feature enabled. Please REBOOT your computer.
+echo After reboot, run: wsl --install -d Ubuntu
+pause
